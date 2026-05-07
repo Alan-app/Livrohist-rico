37bf2445a3468f64f923bcf0a389115188825953
Alanoliveira <uber.alanoliveira@gmail.com>
2026-05-07 15:43:03 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'livro-historico-v23-app-v03-20260507';
+const CACHE_NAME = 'livro-historico-v24-app-v03-livro-fix-20260507-1';
 const APP_ASSETS = ['./','./index.html','./app.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS)));
@@ -10,9 +10,11 @@ self.addEventListener('activate', event => {
 });
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).then(response => {
-    const cloned = response.clone();
-    if (event.request.url.startsWith(self.location.origin)) caches.open(CACHE_NAME).then(cache => cache.put(event.request, cloned));
-    return response;
-  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html'))));
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const cloned = response.clone();
+      if (event.request.url.startsWith(self.location.origin)) caches.open(CACHE_NAME).then(cache => cache.put(event.request, cloned));
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
 });
