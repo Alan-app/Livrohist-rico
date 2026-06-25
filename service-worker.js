7f4e78b228d056b815c5226b252ce71c1bd0aa90
Alanoliveira <uber.alanoliveira@gmail.com>
2026-06-25 20:25:28 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'nautilus-vbeta20-core';
-const RUNTIME_CACHE = 'nautilus-vbeta20-runtime';
+const CACHE_NAME = 'nautilus-vbeta21-core';
+const RUNTIME_CACHE = 'nautilus-vbeta21-runtime';
 const APP_ASSETS = [
   './',
   './index.html',
